b9c5f3bd0e1b0ae070cbeb22230483c84a04d727
hgshkt <nichtojestvozxc@gmail.com>
2024-06-27 17:49:28 +0300

    avatar placed on background

--- app/src/main/java/com/hgshkt/chatproject/presentation/screens/main/profile/ProfileScreen.kt
+++ app/src/main/java/com/hgshkt/chatproject/presentation/screens/main/profile/ProfileScreen.kt
@@ -1,38 +1,42 @@
 package com.hgshkt.chatproject.presentation.screens.main.profile
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hgshkt.chatproject.R
 
 @Composable
-fun ProfileScreen(
-    modifier: Modifier = Modifier
-) {
+fun ProfileScreen() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "Background image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.3f)
+    )
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Background image",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 40.dp)
+        Spacer(
+            modifier = Modifier.fillMaxHeight(0.25f)
         )
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = "Background image",
             modifier = Modifier.size(80.dp)
         )
-
         Text("Name")
     }
 }
