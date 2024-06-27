591c8430de88a5cf1d20a11d12608e4fbfdcd8d2
hgshkt <nichtojestvozxc@gmail.com>
2024-06-27 18:07:01 +0300

    login and registration screen navigation changed

--- app/src/main/java/com/hgshkt/chatproject/presentation/screens/security/registration/RegistrationScreen.kt
+++ app/src/main/java/com/hgshkt/chatproject/presentation/screens/security/registration/RegistrationScreen.kt
@@ -1,15 +1,23 @@
 package com.hgshkt.chatproject.presentation.screens.security.registration
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.hgshkt.chatproject.presentation.navigation.SecurityScreen
+import com.hgshkt.chatproject.presentation.navigation.Screen
 import com.hgshkt.chatproject.presentation.screens.security.LinkToLoginScreen
 import com.hgshkt.chatproject.presentation.screens.security.LoginField
 import com.hgshkt.chatproject.presentation.screens.security.PasswordField
@@ -35,7 +43,7 @@ fun RegistrationScreen(
                 // viewModel.register()
             }
             LinkToLoginScreen {
-                navController.navigate(SecurityScreen.Registration.route)
+                navController.navigate(Screen.Security.Login.route)
             }
         }
     }
