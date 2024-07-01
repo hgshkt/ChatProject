7b82eb394705cd6acf9498385b92779cb0758748
hgshkt <nichtojestvozxc@gmail.com>
2024-07-01 14:30:10 +0300

    add composable to NavGraph

--- app/src/main/java/com/hgshkt/chatproject/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/hgshkt/chatproject/presentation/navigation/NavGraph.kt
@@ -2,8 +2,14 @@ package com.hgshkt.chatproject.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import com.hgshkt.chatproject.presentation.screens.main.chat.ChatScreen
+import com.hgshkt.chatproject.presentation.screens.main.chatList.ChatListScreen
+import com.hgshkt.chatproject.presentation.screens.main.currentProfile.CurrentProfileScreen
+import com.hgshkt.chatproject.presentation.screens.security.login.LoginScreen
+import com.hgshkt.chatproject.presentation.screens.security.registration.RegistrationScreen
 
 @Composable
 fun NavGraph() {
@@ -12,26 +18,45 @@ fun NavGraph() {
     NavHost(navController = controller, startDestination = Screen.Main.route) {
         navigation(
             route = Screen.Main.route,
-            startDestination = Screen.Main.Profile.route
+            startDestination = Screen.Main.CurrentProfile.route
         ) {
-
+            composable(route = Screen.Main.CurrentProfile.route) {
+                CurrentProfileScreen()
+            }
+            composable(route = Screen.Main.ChatList.route) {
+                ChatListScreen()
+            }
+            composable(route = Screen.Main.Chat.route) {
+                ChatScreen(chatId = "")
+            }
+            composable(route = Screen.Main.Profile.route) {
+                ChatScreen(chatId = "")
+            }
         }
         navigation(
             route = Screen.Security.route,
             startDestination = Screen.Security.Login.route
         ) {
-
+            composable(route = Screen.Security.Login.route) {
+                LoginScreen(controller)
+            }
+            composable(route = Screen.Security.Login.route) {
+                RegistrationScreen(controller)
+            }
         }
     }
 }
 
 sealed class Screen(val route: String) {
     data object Main: Screen("Main") {
+
+        data object CurrentProfile: Screen("CurrentProfile")
         data object Profile: Screen("Profile")
         data object ChatList: Screen("ChatList")
         data object Chat: Screen("Chat")
     }
     data object Security: Screen("Security") {
+
         data object Login: Screen("Login")
         data object Registration: Screen("Registration")
     }
