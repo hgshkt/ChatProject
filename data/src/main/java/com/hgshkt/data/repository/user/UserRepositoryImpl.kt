3358c991dd3ef5471441742cbcc2d9a51e1a6155
hgshkt <nichtojestvozxc@gmail.com>
2024-07-04 10:41:48 +0300

    add getUserById() function to LocalUserStorage and injecting it to UserRepositoryImpl

--- data/src/main/java/com/hgshkt/data/repository/user/UserRepositoryImpl.kt
+++ data/src/main/java/com/hgshkt/data/repository/user/UserRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.hgshkt.data.repository.user
 
+import com.hgshkt.data.mapper.toDomain
 import com.hgshkt.data.mapper.toRepositoryResponse
 import com.hgshkt.data.storage.user.interfaces.LocalUserStorage
 import com.hgshkt.data.storage.user.interfaces.RemoteUserStorage
+import com.hgshkt.data.storage.user.model.StorageUser
 import com.hgshkt.domain.data.repository.UserRepository
+import com.hgshkt.domain.model.User
 
 class UserRepositoryImpl(
     private val remoteUserStorage: RemoteUserStorage,
@@ -11,6 +14,12 @@ class UserRepositoryImpl(
 ): UserRepository {
 
     override suspend fun getUserById(id: String): UserRepository.GetUserByIdResponse {
+        // if user saved locally,return it
+        val localUser = localUserStorage.getUserById(id)
+        localUser?.let {
+            return UserRepository.GetUserByIdResponse.Success(it.toDomain())
+        }
+        // in other case fetch value from remote storage
         return remoteUserStorage.getUserById(id).toRepositoryResponse()
     }
 
