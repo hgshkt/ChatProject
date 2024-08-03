15cb75fb7d828336de6e931b66fe930d1dd64468
hgshkt <nichtojestvozxc@gmail.com>
2024-08-03 22:56:47 +0300

    added friendList, incomingInvitations, outcomingInvitations as user model fields

--- domain/src/main/java/com/hgshkt/domain/model/User.kt
+++ domain/src/main/java/com/hgshkt/domain/model/User.kt
@@ -4,5 +4,8 @@ data class User(
     val id: String,
     val name: String,
     val avatarUrl: String?,
-    val backgroundUrl: String?
+    val backgroundUrl: String?,
+    val friendList: List<String>,
+    val incomingInvitations: List<String>,
+    val outcomingInvitations: List<String>
 )
